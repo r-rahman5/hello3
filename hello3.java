33d6ccbd9e978fa81e951a1a0d6ca2e8b44fcf02
Riaz Rahman <riazrahman@Riazs-MacBook-Pro-2.local>
2024-03-12 17:13:34 +0000

    add new feature

--- hello3.java
+++ hello3.java
@@ -1,7 +1,7 @@
 public class hello{
     public static void main(String[] args){
         System.out.println("Hello World");
-		System.out.println("GitHub");
+		System.out.println("new GitHub");
     }
 	public static void newMethod(){
 		System.out.println("new method");
