871753c7c65d3ea5b31a4c807073040689c3e357
Riaz Rahman <riazrahman@Riazs-MacBook-Pro-2.local>
2024-03-12 16:55:12 +0000

    Add hello3.java

---
+++ hello3.java
@@ -0,0 +1,9 @@
+public class hello{
+    public static void main(String[] args){
+        System.out.println("Hello World");
+		System.out.println("GitHub");
+    }
+	public static void newMethod(){
+		System.out.println("new method");
+	}
+}
